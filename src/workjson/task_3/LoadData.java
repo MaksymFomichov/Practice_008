a2e3432a0bce9ceeab5f2bd26d9e645cc908c7c1
Maksym <m.fomichov@gmail.com>
2017-09-07 12:55:23 +0300

    сохраняем

--- src/workjson/task_3/LoadData.java
+++ src/workjson/task_3/LoadData.java
@@ -18,20 +18,4 @@ public class LoadData {
             System.out.println(key + " " + data.get(key));
         }
     }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public HashMap<String, String> getData() {
-        return data;
-    }
-
-    public void setData(HashMap<String, String> data) {
-        this.data = data;
-    }
 }
